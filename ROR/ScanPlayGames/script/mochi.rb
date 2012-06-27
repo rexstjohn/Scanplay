838eb75e661de2417385852abbff30cce70cad19
rexstjohn <rexstjohn@gmail.com>
2012-06-26 21:30:13 -0700

    stupid sql bullshit going on here

--- ROR/ScanPlayGames/script/mochi.rb
+++ ROR/ScanPlayGames/script/mochi.rb
@@ -3,71 +3,34 @@ require 'rubygems'
 require 'net/http'
 require 'uri'
 require 'HTTParty'
+require 'json'
+require 'active_record'
+require '../app/models/Game'
 
-#note to self: must gem install httparty or this wont work
-=begin
-# Use the class methods to get down to business quickly
-response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
-puts response.body, response.code, response.message, response.headers.inspect
-
-response.each do |item|
-  puts item['user']['screen_name']
-end
-
-# Or wrap things up in your own class
-class Twitter
-  include HTTParty
-  base_uri 'twitter.com'
-
-  def initialize(u, p)
-    @auth = {:username => u, :password => p}
-  end
-
-  # which can be :friends, :user or :public
-  # options[:query] can be things like since, since_id, count, etc.
-  def timeline(which=:friends, options={})
-    options.merge!({:basic_auth => @auth})
-    self.class.get("/statuses/#{which}_timeline.json", options)
-  end
-
-  def post(text)
-    options = { :query => {:status => text}, :basic_auth => @auth }
-    self.class.post('/statuses/update.json', options)
-  end
-end
-
-twitter = Twitter.new("rexstjohn@gmail.com", "3w0k5r00l!")
-puts twitter.timeline
-=end
-
-#whatever game url we need to pull the feed from
-#base_uri = 'http://catalog.mochimedia.com/feeds/query/?q=recommendation%3A%3E%3D3&partner_id=2a451cb43fb275ac&limit=11851'
-
-
-#note to self: must gem install httparty or this wont work
-
-# Use the class methods to get down to business quickly
-#response = HTTParty.get(base_uri)
-#puts response.body, response.code, response.message, response.headers.inspect
-
-#response.each do |item|
- # puts item
-#end
-# Or wrap things up in your own class
 class Mochi
   include HTTParty
   base_uri 'catalog.mochimedia.com'
 
-  attr_accessor :partner_id
+  def partner_id
+    '2a451cb43fb275ac'
+  end
+
+  def limit
+    '11851'
+  end
+
+  def recommendation
+    ':>=3'
+  end
+
 
   def initialize
-    @partner_id = '2a451cb43fb275ac'
   end
 
   # which can be :friends, :user or :public
   # options[:query] can be things like since, since_id, count, etc.
   def get_feed
-    options = { :query => {:recommendation => ':>=3'}, :partner_id => @partner_id, :limit => '11851' }
+    options = { :query => {:recommendation => recommendation}, :partner_id => self.partner_id, :limit => self.limit }
     self.class.get("/feeds/query/",options)
   end
 
@@ -75,24 +38,72 @@ class Mochi
     #self.class.get("/statuses/#{which}_timeline.json", options)
   end
 
+  #downloads game zips to a given directory.
   def get_game_zips
       
   end
 
+  #enumerates the games in a feed
   def enumerate_games
     response = get_feed
+    games_key = 'games'
 
-    response['games'].each do |game|
-
-      game.each do |k,i|
-        puts "key: #{k} value: #{i}"
+    response[games_key].each do |game|
+      game.each do |k,v|
+        puts "key: #{k} value: #{v}"
       end
+    end
+  end
+
+  #generates an active record model and migration from the json provided
+  def generate_model
+    response = get_feed
+    games_key = 'games'
+    json_attrs = Array.new#empty array of attributes
+
+    response[games_key].each do |game|
+
+      #step 1: generate the accessor list
+      #step 2: generate the template migration
+      print "attr_accessor "
+      
+      game.each do |k,i|
+        print ":#{k}, "
+
+        #shove that attribute in our array here
+        json_attrs << ":" + k
+      end
+
+      puts
+      puts "create_table :games do |t|"
+
+      json_attrs.each do |atr|
+          puts "t.string #{atr}"
+      end
+
+      puts "t.timestamps"
+      puts "end"
+
+      return
+    end
+  end
+
+  #imports a game list and stuffs it in the database
+  def import_games
+    response = get_feed
+    games_key = 'games'
+
+    #create games for the database
+    response[games_key].each do |game|
+      #puts game
+      Game.create(game)
     end
   end
 end
 
 #fetch the goodness
 mochi = Mochi.new
-mochi.enumerate_games
+#mochi.generate_model
 #puts mochi.get_feed
+mochi.import_games
 
