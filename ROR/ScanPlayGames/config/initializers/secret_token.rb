c2186a9715f03c21815dca40f54ee4040f386434
rexstjohn <rexstjohn@gmail.com>
2013-11-17 11:23:32 -0800

    organizing by language

--- ROR/ScanPlayGames/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ScanPlayGames::Application.config.secret_token = '64c253e47ee2d86151a52097ff5455b95d359b171026bc6ac689a14c484b8cf2108f5ed00676af1a2d5fcc38801809a4d4a6de83097aa9b334c03e65946a2cea'
