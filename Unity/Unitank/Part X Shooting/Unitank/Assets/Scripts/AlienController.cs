67cf68ee91196b76d08dedda0262c6fddb951348
rexstjohn <rexstjohn@Rexs-MacBook-Air.local>
2013-12-26 17:16:34 -0800

    unity part X

---
+++ Unity/Unitank/Part X Shooting/Unitank/Assets/Scripts/AlienController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienController : MonoBehaviour {
+
+	public float alienSpeed = .02f;
+	private ScoreController score;
+	
+	// Use this for initialization
+	void Start () { 
+		score = GameObject.Find("Score").GetComponent<ScoreController>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		transform.position += new Vector3(0, -alienSpeed, 0);
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the alien hits the trigger...
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+
+			if(score.score >= 100)
+				score.score -= 100;
+			else if(score.score > 0)
+				score.score = 0;
+
+		} else if (col.gameObject.tag == "Terrain") {
+			score.score += 100;
+			Destroy (gameObject);
+		}else if (col.gameObject.tag == "Pew") {
+			score.score += 100;
+			Destroy (gameObject);
+		}
+	}
+
+}
---
+++ Unity/Unitank/Part X Shooting/Unitank/Assets/Scripts/TurretController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour {
+
+	private Vector3 mouse_pos;
+	private Vector3 object_pos;
+	private float angle;
+	private float bulletSpeed = 500;
+	public GameObject[] ammo;		// Array of enemy prefabs.
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update(){ 
+	}
+
+	void FixedUpdate () {    
+		// Point the cannon at the mouse.
+		mouse_pos = Input.mousePosition;
+		mouse_pos.z = 0.0f; 
+		object_pos = Camera.main.WorldToScreenPoint(transform.position);
+		mouse_pos.x = mouse_pos.x - object_pos.x;
+		mouse_pos.y = mouse_pos.y - object_pos.y;
+		angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg - 90;
+		Vector3 rotationVector = new Vector3 (0, 0, angle);
+		transform.rotation = Quaternion.Euler(rotationVector);
+
+		// Fire a bullet.
+		if(Input.GetMouseButtonDown(0)){
+			int ammoIndex = 0; 
+			GameObject bullet = (GameObject)Instantiate(ammo[ammoIndex], transform.position, transform.rotation);
+			bullet.transform.LookAt(mouse_pos);
+			bullet.rigidbody2D.AddForce(bullet.transform.forward * bulletSpeed);
+		}
+	}
+}
