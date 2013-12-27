5a7b35f11f78f191018d11133b42975e2bb96d1f
rexstjohn <rexstjohn@Rexs-MacBook-Air.local>
2013-12-26 16:15:38 -0800

    updating unity ix tutorial

---
+++ Unity/Unitank/Part IX Spawning/Unitank/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	public int score = 0;					// The player's score. 
+	private int previousScore = 0;			// The score in the previous frame.
+	
+	void Awake ()
+	{
+	}
+	
+	void Update ()
+	{
+		// Set the score text.
+		guiText.text = "Score: " + score;
+		
+		// Set the previous score to this frame's score.
+		previousScore = score;
+	}
+}
---
+++ Unity/Unitank/Part IX Spawning/Unitank/Assets/Scripts/TurretController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour {
+
+	private Vector3 mouse_pos;
+	private Vector3 object_pos;
+	private float angle;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update(){ 
+	}
+
+	void FixedUpdate () {    
+		//
+		mouse_pos = Input.mousePosition;
+		mouse_pos.z = 0.0f; 
+		object_pos = Camera.main.WorldToScreenPoint(transform.position);
+		mouse_pos.x = mouse_pos.x - object_pos.x;
+		mouse_pos.y = mouse_pos.y - object_pos.y;
+		angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg - 90;
+		Vector3 rotationVector = new Vector3 (0, 0, angle);
+		transform.rotation = Quaternion.Euler(rotationVector);
+	}
+}
